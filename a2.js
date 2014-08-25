1466a7a8f1ebdec9cd11416bcd702bf83b117072
ankita021190 <ankita.it.2012@gmail.com>
2014-08-25 12:46:16 +0530

    a2 done

--- a2.js
+++ a2.js
@@ -1,13 +1,20 @@
 var randomwords = require('random-words');
 var fs = require('fs');
+var count ;
 
-fs.appendFile("t2.txt", randomwords() +'\n', function(words){
-	if(words){
-		console.log(words);
+fs.appendFile("t2.txt", randomwords() +'\n', function(isError) {
+	if(isError) {
+		console.log(isError);
 	}
-	else{
-		console.log("Words are generating");
+	else {
+		console.log("Sucessfully wrote to the file, words are generating");
 	}
 
+	fs.readFile("t2.txt",'Utf8', function(isError, data) 
+	{   
+		var file_content_array = data.split('\n');
+		console.log("this number of times :");
+		console.log(file_content_array.length);
+	});
 
-});
+}); // Ends appendFile
